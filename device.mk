6b3f925080d7c69af2336777a22b6fbad761224e
Yan Yan <evitayan@google.com>
2025-04-26 14:37:21 +0545

    onyx: Add feature flag for IPSEC_TUNNEL_MIGRATION This commit adds the IPSEC_TUNNEL_MIGRATION feature flag, with XFRM_MIGRATE used as the implementation
    Bug: 273379472
    Test: build, flash and verify with "adb shell pm list features"
    Test: atest IpSecManagerTunnelTest
    Change-Id: Id2ee3e65183e32ea63f6251ba921e1e4c6b09ed0
    Signed-off-by: romgharti <romgharti@gmail.com>
    Signed-off-by: Sachin <sachinbarange86@gmail.com>

--- device.mk
+++ device.mk
@@ -333,6 +333,10 @@ PRODUCT_PACKAGES += \
     vendor_firmware_mnt_mountpoint \
     vendor_modem_firmware_mountpoint
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
