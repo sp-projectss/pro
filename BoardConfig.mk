6efe87ea552d3bac575e7739cb50fb7fa078d535
Alexander Koskovich <akoskovich@pm.me>
2025-11-17 10:06:45 +0000

    onyx: Remove virtual framebuffer  * It's largely for testing purposes and it consumes    a large amount of memory. If your hardware doesn't    have graphics then it can be useful, which isn't the    case for shipped devices.
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9
    Signed-off-by: romgharti <romgharti@gmail.com>
    Signed-off-by: Sachin <sachinbarange86@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,7 +91,6 @@ TARGET_KERNEL_CONFIG := \
     vendor/onyx_perf.config \
 
 BOARD_KERNEL_CMDLINE := \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
     sysctl.kernel.firmware_config.force_sysfs_fallback=1 \
     swinfo.fingerprint=$(LINEAGE_VERSION) \
     mtdoops.fingerprint=$(LINEAGE_VERSION) \
