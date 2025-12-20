52b228f129df7a3394818e225366e3e8ea3c16e6
k4lchakra <k4lchakra@gmail.com>
2025-12-20 23:15:47 +0545

    onyx: patch libcom.xiaomi.metadatautils with libui-v34
    * without this patch miuicamera force close
    
    Signed-off-by: k4lchakra <k4lchakra@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -292,8 +292,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed(
             'android.hardware.graphics.common-V5-ndk.so',
             'android.hardware.graphics.common-V7-ndk.so'
-    ),
-
+        ),
+    'vendor/lib64/libcom.xiaomi.metadatautils.so': blob_fixup()
+        .add_needed('libui-v34.so'),
     'vendor/lib64/libwfdmmsrc_proprietary.so': blob_fixup()
         .replace_needed(
             'android.media.audio.common.types-V2-ndk.so',
