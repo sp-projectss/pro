3214f77753422d6a1da7263481f1ce13cb0584e7
unmoved21 <unmoved2121@gmail.com>
2026-02-22 18:58:01 +0000

    onyx: Bring back dolby audio
    Change-Id: If5a135f484c85a109d6828530ac5ed4f315ae282
    Signed-off-by: Byben <heybyben@gmail.com>
    Signed-off-by: romgharti <romgharti@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -202,9 +202,6 @@ blob_fixups: blob_fixups_user_type = {
             'libtinyxml2-v34.so'
     ),
 
-    'vendor/etc/clstc_config_library.xml': blob_fixup()
-        .regex_replace(r'<library>\s*<name>libdolbyclstc[\s\S]*?</library>', ''),
-
     'vendor/etc/sensors/hals.conf': blob_fixup()
         .regex_replace('.*vl53l8.*\n?', ''),
 
@@ -222,24 +219,16 @@ blob_fixups: blob_fixups_user_type = {
     ),
 
     (
-        'vendor/lib64/hw/android.hardware.bluetooth.audio_sw.so',
-        'vendor/lib64/hw/libaudioeffecthal.qti.so',
-        'vendor/lib64/libaudio_aidl_conversion_common_ndk_prebuilt.so',
-        'vendor/lib64/libaudioserviceexampleimpl.so',
-        'vendor/lib64/soundfx/libbundleaidl.so',
-        'vendor/lib64/soundfx/libdownmixaidl.so',
-        'vendor/lib64/soundfx/libdynamicsprocessingaidl.so',
-        'vendor/lib64/soundfx/libloudnessenhanceraidl.so',
-        'vendor/lib64/soundfx/libqcompostprocbundle.so',
-        'vendor/lib64/soundfx/libqcomvisualizer.so',
-        'vendor/lib64/soundfx/libqcomvoiceprocessing.so',
-        'vendor/lib64/soundfx/libreverbaidl.so',
-        'vendor/lib64/soundfx/libvisualizeraidl.so',
-        'vendor/lib64/soundfx/libvolumelistener.so'
+        'vendor/lib64/soundfx/liblvacfsprocessingaidl.so',
+        'vendor/lib64/soundfx/libdlbvolaidl.so',
     ): blob_fixup()
         .replace_needed(
-            'android.media.audio.common.types-V6-ndk.so',
+            'android.media.audio.common.types-V4-ndk.so',
             'android.media.audio.common.types-V3-ndk.so'
+    )
+        .replace_needed(
+            'android.hardware.audio.effect-V2-ndk.so',
+            'android.hardware.audio.effect-V3-ndk.so'
     ),
 
     (
