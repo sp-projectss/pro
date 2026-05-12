a3ee006ec7cfca54444eb2b2b251f7da26d4c334
AdarshGrewal <adarshgrewal@gmail.com>
2026-05-12 22:59:09 +0530

    onyx: Force disable REDIR_PARTY_NUM_SUPPORT
     * edit it to go from setFeature(5, 1) to setFeature(5, 0)
    
     * Xiaomi's libqcrilNrVoiceModule.so overrides VoiceModule::initFeatureMask()
     to unconditionally force REDIR_PARTY_NUM_SUPPORT (feature 5) to true,
     regardless of the qcril DB setting. This causes the redirecting party
     number to be appended to the displayed call number
    
    Change-Id: If87fce53ed0e3b500173631b9bec0cc58ca9a5b2
    Signed-off-by: romgharti <romgharti@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -261,6 +261,9 @@ blob_fixups: blob_fixups_user_type = {
             'libbluetooth_audio_session_aidl_prebuilt.so'
     ),
 
+    'vendor/lib64/libqcrilNrVoiceModule.so': blob_fixup()
+        .sig_replace('a1 00 80 52 22', 'a1 00 80 52 02'),
+
     (
         'vendor/lib64/libVoiceSdk.so',
         'vendor/lib64/libcapiv2uvvendor.so',
