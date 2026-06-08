46f9faf810daf11cb191b8caff625d9fe3b67bad
Sachinpawar86 <sachinbarange86@gmail.com>
2026-06-08 14:03:41 +0530

    onyx: configs: Move dax-default.xml to device tree
    Signed-off-by: Sachin <sachinbarange86@gmail.com>

--- device.mk
+++ device.mk
@@ -155,6 +155,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LunarisDolby
 
+# Dolby
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
