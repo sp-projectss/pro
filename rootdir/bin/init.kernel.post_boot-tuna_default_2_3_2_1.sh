92b4d841c825b960416b50bc2c91b720fdc9c380
romgharti <romgharti@gmail.com>
2026-08-10 13:37:00 +0000

    onyx: rootdir: Adjust cpuset parameters * Reduce cpu core to foreground, background, system-background * Keep cpu low usage and save battery
    Signed-off-by: romgharti <romgharti@gmail.com>
    Signed-off-by: Sachin <sachinbarange86@gmail.com>

--- rootdir/bin/init.kernel.post_boot-tuna_default_2_3_2_1.sh
+++ rootdir/bin/init.kernel.post_boot-tuna_default_2_3_2_1.sh
@@ -180,9 +180,9 @@ fi
 echo 0 > /proc/sys/kernel/sched_util_clamp_min_rt_default
 
 # cpuset parameters
-echo 0-6 > /dev/cpuset/foreground/cpus
-echo 0-2 > /dev/cpuset/background/cpus
-echo 0-2 > /dev/cpuset/system-background/cpus
+echo 0-5 > /dev/cpuset/foreground/cpus
+echo 0-1 > /dev/cpuset/background/cpus
+echo 0-1 > /dev/cpuset/system-background/cpus
 echo 0-1 > /dev/cpuset/restricted/cpus
 
 # configure bus-dcvs
